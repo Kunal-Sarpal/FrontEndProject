42f7762033cbe1c76cd759a99549cff30546df1b
Kunal-Sarpal <sarpalkunal7@gmail.com>
2024-04-20 13:54:56 +0530

    Frontend update 20

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,46 +1,218 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
+import React, { useState } from "react";
 import { FaRegBell } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import { IoBagHandleOutline } from "react-icons/io5";
 import { CiBag1 } from "react-icons/ci";
 
-
-
 function Navbar() {
+  const [activeTab, setActiveTab] = useState("home");
+  const [isHovered, setIsHovered] = useState(null);
+  const [showPhotos, setShowPhotos] = useState(false);
+
+  const categories = [
+    "Topwear",
+    "T-Shirts",
+    "Casual Shirts",
+    "Formal Shirts",
+    "Sweatshirts",
+    "Sweaters",
+    "Jackets",
+    "Blazers & Coats",
+    "Suits",
+    "Rain Jackets",
+    "Indian & Festive Wear",
+    "Kurtas & Kurta Sets",
+    "Sherwanis",
+    "Nehru Jackets",
+    "Dhotis",
+    "Bottomwear",
+    "Jeans",
+    "Casual Trousers",
+    "Formal Trousers",
+    "Shorts",
+    "Track Pants & Joggers",
+    "Innerwear & Sleepwear",
+    "Briefs & Trunks",
+    "Boxers",
+    "Vests",
+    "Sleepwear & Loungewear",
+    "Thermals",
+    "Plus Size",
+    "Footwear",
+    "Casual Shoes",
+    "Sports Shoes",
+    "Formal Shoes",
+    "Sneakers",
+    "Sandals & Floaters",
+    "Flip Flops",
+    "Socks",
+    "Personal Care & Grooming",
+    "Sunglasses & Frames",
+    "Watches",
+    "Sports & Active Wear",
+    "Sports Shoes",
+    "Sports Sandals",
+    "Active T-Shirts",
+    "Track Pants & Shorts",
+    "Tracksuits",
+    "Jackets & Sweatshirts",
+    "Sports Accessories",
+    "Swimwear",
+    "Gadgets",
+    "Smart Wearables",
+    "Fitness Gadgets",
+    "Headphones",
+    "Speakers",
+    "Fashion Accessories",
+    "Wallets",
+    "Belts",
+    "Perfumes & Body Mists",
+    "Trimmers",
+    "Deodorants",
+    "Ties, Cufflinks & Pocket Squares",
+    "Accessory Gift Sets",
+    "Caps & Hats",
+    "Mufflers, Scarves & Gloves",
+    "Phone Cases",
+    "Rings & Wristwear",
+    "Helmets",
+    "Bags & Backpacks",
+    "Luggages & Trolleys",
+  ];
+
+  const handleTabClick = (tab) => {
+    setActiveTab(tab);
+  };
+
   return (
-    <div className="w-full px-3 md:px-0">
-      <div className="flex justify-between py-2 items-center ">
-        <div className=" gap-8 justify-center items-center hidden md:flex">
-          <div>Home</div>
-          <div className="font-bold">
-            Shop <span className="block trasnlate-ori"></span>
+    <div className="relative">
+      <div className="w-full px-3 md:px-0">
+        <div className="flex justify-between py-2 items-center">
+          <div className="gap-8 justify-center items-center hidden md:flex">
+            <div
+              onMouseEnter={() => setIsHovered("shop")}
+              onMouseLeave={() => setIsHovered(null)}
+              className={
+                activeTab === "shop"
+                  ? "font-normal cursor-pointer relative border-t-4 "
+                  : "cursor-pointer relative"
+              }
+            >
+              <div className="text-sm font-normal">SHOP</div>
+              {isHovered === "shop" && (
+                <div className="absolute top-full left-0 bg-white w-[60vw] h-fit border  border-gray-300 shadow-lg rounded-md p-4 grid grid-cols-4 gap-2 duration-200">
+                  {categories.map((category, index) => (
+                    <a
+                      key={index}
+                      href="#"
+                      className={
+                        [
+                          "Topwear",
+                          "T-Shirts",
+                          "Casual Shirts",
+                          "Gadgets",
+                          "Fashion Accessories",
+                          "Formal Shirts",
+                        ].includes(category)
+                          ? "text-red-500 text-lg font-semibold"
+                          : "text-xs  "
+                      }
+                    >
+                      {category}
+                    </a>
+                  ))}
+                </div>
+              )}
+            </div>
+            <div
+              className="text-lg text-red-500  relative"
+              onMouseEnter={() => setShowPhotos(true)}
+              onMouseLeave={() => setShowPhotos(false)}
+            >
+              <div className="text-sm font-normal">STUDIO</div>
+              {showPhotos && (
+                <div className="absolute top-full left-0 bg-white border border-gray-300 w-96 h-96 shadow-lg rounded-md p-2 grid duration-200">
+                  <div className="text-zinc-500">Explore New Fashion 
+                  <p className="text-sm">Your daily inspiration for everything fashion</p>  
+                  </div>
+                
+                  <img className="w-full h-full"
+                    src="https://constant.myntassets.com/web/assets/img/sudio-nav-banner.png"
+                    alt="Studio"
+                  />
+                  <button className="text-sm border-[2px] border-zinc-500 rounded-lg m-1 text-zinc-500">Explore {"->"}</button>
+                </div>
+              )}
+            </div>
+            <div
+              onMouseEnter={() => setIsHovered("about")}
+              onMouseLeave={() => setIsHovered(null)}
+              onClick={() => handleTabClick("about")}
+              className={
+                activeTab === "about"
+                  ? "font-normal cursor-pointer relative"
+                  : "cursor-pointer relative"
+              }
+            >
+             <div className="text-sm font-normal">ABOUT</div>
+              {isHovered === "about" && (
+                <div className="absolute top-full left-0 bg-white w-64 h-64 border border-gray-300 shadow-lg rounded-md p-4">
+                  <div>About Us</div>
+                  <div>Our Team</div>
+                </div>
+              )}
+            </div>
+           
+            <div
+              onMouseEnter={() => setIsHovered("contact")}
+              onMouseLeave={() => setIsHovered(null)}
+              onClick={() => handleTabClick("contact")}
+              className={
+                activeTab === "contact"
+                  ? "font-normal cursor-pointer relative"
+                  : "cursor-pointer relative"
+              }
+            >
+              <div className="text-sm font-normal">CONTACT</div>
+              {isHovered === "contact" && (
+                <div className="absolute top-full left-0 bg-white w-64 h-64 border border-gray-300 shadow-lg rounded-md p-4">
+                  <h1 className="text-red-500 text-xl font-semibold">{`Kunal Production's`}</h1>
+                  <div className="text-sm">sarpalkunal7@gmail.com</div>
+                  <div className="text-sm">+91 7814505429</div>
+                  <div className="border-[1px] p-3 mt-28 border-zinc-500 rounded-md justify-center items-center flex  text-sm bg-red-500 text-white">Contact Us</div>
+                </div>
+              )}
+            </div>
           </div>
-          <div>About</div>
-          <div>Contact</div>
-        </div>
-        <div className="flex gap-2">
-          <div className="text-2xl flex justify-center items-center">
-            <CiBag1 />
+          
+          <div className="flex gap-2">
+            <div className="text-xl flex justify-center items-center">
+              <CiBag1 />
+            </div>
+            <div className="font-bold text-lg animate-pulse">3NT3.FASHION</div>
           </div>
-          <div className="font-bold text-lg">GLAMORA</div>
-        </div>
-        <div className="flex gap-6 justify-center items-center">
-          <div className="text-xl text-zinc-500">
-            <IoSearch />
+          <div className="flex gap-6 justify-center items-center">
+          
+            <div className="text-xl text-zinc-500">
+              <IoSearch />
+            </div>
+            <div className="text-lg text-zinc-500">
+              <FaRegBell />
+            </div>
+            <div>
+              <button className="border-[1px] border-zinc-400 p-1 px-2 rounded-full w-28 flex items-center justify-center gap-3 text-md font-semibold text-zinc-600">
+                <div>My Cart </div>
+                <div>
+                  <IoBagHandleOutline />
+                </div>
+              </button>
+            </div>
+            <div
+              className="w-8 h-8 border-[1px] border-black rounded-full"
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+            ></div>
           </div>
-          <div className="text-lg text-zinc-500">
-            <FaRegBell />
-          </div>
-          <div>
-            <button className="border-[1px] border-zinc-400 p-1 px-2 rounded-full w-28 flex items-center justify-center gap-3 text-md font-semibold text-zinc-600 ">
-              <div>My Cart </div>
-              <div>
-                <IoBagHandleOutline />{" "}
-              </div>
-            </button>
-          </div>
-          <div className="w-8 h-8 border-[1px] border-black rounded-full"></div>
         </div>
       </div>
     </div>
